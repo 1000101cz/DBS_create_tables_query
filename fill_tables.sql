393e34dcc9006aaef8d9d910a58445f0fdb852c9
Štěpán Marousek <71834145+1000101cz@users.noreply.github.com>
2023-05-08 21:05:06 +0200

    Create fill_tables.sql

---
+++ fill_tables.sql
@@ -0,0 +1,6 @@
+INSERT INTO manufacturingplant (street, city, zip, established) VALUES ('U Kastanu 54', 'Prague', 16900, '1981-02-18');
+INSERT INTO manufacturingplant (street, city, zip, established) VALUES ('Cerna Pole 18', 'Brno', 26100, '1989-10-10');
+INSERT INTO manufacturingplant (street, city, zip, established) VALUES ('F. Sramka 12/368', 'Jihlava', 40202, '2005-05-01');
+INSERT INTO manufacturingplant (street, city, zip, established) VALUES ('Spinava 55', 'Liberec', 56821, '1998-07-24');
+INSERT INTO manufacturingplant (street, city, zip, established) VALUES ('Vetruse 223', 'Usti nad Labem', 68211, '1986-01-13');
+
